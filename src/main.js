37e96af3d89ce2e5a78dcd37d5a1069a1ffb980f
Jecheon Ryu <rjc1704@gmail.com>
2023-06-05 01:39:12 +0900

    refactor: event.preventDefault() 제거

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ window.addEventListener("hashchange", renderPage);
 // 2. 사이드바에서 페이지이동 버튼을 클릭 시 해당 페이지를 렌더링한다.
 const sidebar = document.querySelector(".sidebar");
 sidebar.addEventListener("click", (event) => {
-  event.preventDefault();
   if (event.target === sidebar) return;
 
   window.location.hash = event.target.hash;
